c9d9a319a814ba9ec2081362a0f93fa4c2e935c3
mingoarte <domingoarte2850@gmail.com>
2018-05-17 22:20:29 -0400

    Agregando las clases del cpu y procesos.

---
+++ proyectoI/cpu.py
@@ -0,0 +1,21 @@
+from process import *
+
+class cpu:
+	
+	def __init__(self, pk, quantum = 4):
+		self.pk = pk
+		self.quantum = quantum
+		self.process = None
+
+	def clock(self):
+		tmp = self.process.min_t + self.quantum
+		if self.process.required_t < tmp:
+			self.process.min_t = self.process.required_t
+		else:
+			self.process.min_t = tmp
+
+
+	def free(self)	:
+		tmp = self.process
+		self.process = None
+		return tmp
---
+++ proyectoI/process.py
@@ -0,0 +1,12 @@
+class process:
+
+	def __init__(self, pid, required_t, min_t):
+		self.pid = pid
+		self.required_t = required_t
+		self.min_t = min_t
+
+	def done():
+		if  (self.required_t == self.min_t):
+			return True
+		else:
+			return False
