a763f333ff2ebebf7ba0d7a624c150fb43879c44
mingoarte <domingoarte2850@gmail.com>
2018-06-02 18:44:26 -0400

    Agregando valores default al min_t

--- proyectoI/process.py
+++ proyectoI/process.py
@@ -1,9 +1,12 @@
 class Process():
 
-	def __init__(self, pid, required_t, min_t):
-		self.pid = pid
+	pid_counter = 0
+
+	def __init__(self, required_t, min_t=0):
+		self.pid = self.pid_counter
 		self.required_t = required_t
 		self.min_t = min_t
+		Process.pid_counter += 1
 
 	def done(self):
 		return self.required_t == self.min_t
