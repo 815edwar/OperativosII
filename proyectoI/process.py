24fcf504cb6548aea5804b44204969483395f68a
Edwar Yepez <edwarypz@gmail.com>
2018-06-02 18:43:16 -0400

    Agregadas funciones de comparacion a clase Procesp=o

--- proyectoI/process.py
+++ proyectoI/process.py
@@ -7,4 +7,14 @@ class Process():
 
 	def done(self):
 		return self.required_t == self.min_t
+
+
+	def __eq__(self, other):
+		return self.min_t == other.min_t
+
+	def __lt__(self, other):
+		return self.min_t < other.min_t
+
+	def __gt__(self, other):
+		return self.min_t > other.min_t
 	
